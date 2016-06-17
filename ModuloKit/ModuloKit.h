a4a71eb203ad95741ead78d2afdb71363bb64846
Brandon Sneed <brandon@redf.net>
2016-06-17 13:24:53 -0700

    Project refactor.

---
+++ ModuloKit/ModuloKit.h
@@ -0,0 +1,19 @@
+//
+//  ModuloKit.h
+//  ModuloKit
+//
+//  Created by Brandon Sneed on 6/17/16.
+//  Copyright © 2016 TheHolyGrail. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ModuloKit.
+FOUNDATION_EXPORT double ModuloKitVersionNumber;
+
+//! Project version string for ModuloKit.
+FOUNDATION_EXPORT const unsigned char ModuloKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModuloKit/PublicHeader.h>
+
+
