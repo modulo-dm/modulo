9d30109f453cea579cccd92630957562f5d75f1e
Brandon Sneed <brandon@redf.net>
2016-06-17 13:18:21 -0700

    Initial Commit

---
+++ modulo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  modulo
+//
+//  Created by Brandon Sneed on 6/17/16.
+//  Copyright © 2016 TheHolyGrail. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
